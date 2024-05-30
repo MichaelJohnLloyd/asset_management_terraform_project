bbc523b2efaaad1fa63a69fe86a16722260ac1ef
Michael Lloyd <drumhandsmike@gmail.com>
2024-05-30 15:09:54 +0100

    Changes to initial commit

--- windows_server/snipe_it_server.tf
+++ windows_server/snipe_it_server.tf
@@ -56,13 +56,13 @@ resource "azurerm_network_security_group" "snipe_nsg" {
 resource "azurerm_network_interface" "snipe_nic" {
   name                = "${random_pet.prefix.id}-nic"
   location            = azurerm_resource_group.snipe_it_rg.location
-  resource_group_name = azurerm_resource_group.snipe_it_rgname
+  resource_group_name = azurerm_resource_group.snipe_it_rg.name
 
   ip_configuration {
     name                          = "my_nic_configuration"
-    subnet_id                     = azurerm_subnet.my_terraform_subnet.id
+    subnet_id                     = azurerm_subnet.snipe_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.my_terraform_public_ip.id
+    public_ip_address_id          = azurerm_public_ip.snipe_public_ip.id
   }
 }
 
@@ -82,7 +82,7 @@ resource "azurerm_storage_account" "snipe_storage_account" {
 
 resource "random_id" "random_id" {
   keepers = {
-    resource_group = azurerm_resource_group.rg.name
+    resource_group = azurerm_resource_group.snipe_it_rg.name
   }
 
   byte_length = 8
--- windows_server/virtual_machine.tf
+++ windows_server/virtual_machine.tf
@@ -2,9 +2,9 @@ resource "azurerm_windows_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
   admin_username        = "azureuser"
   admin_password        = random_password.password.result
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.my_terraform_nic.id]
+  location              = azurerm_resource_group.snipe_it_rg.location
+  resource_group_name   = azurerm_resource_group.snipe_it_rg.name
+  network_interface_ids = [azurerm_network_interface.snipe_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
@@ -22,6 +22,6 @@ resource "azurerm_windows_virtual_machine" "main" {
 
 
   boot_diagnostics {
-    storage_account_uri = azurerm_storage_account.my_storage_account.primary_blob_endpoint
+    storage_account_uri = azurerm_storage_account.snipe_storage_account.primary_blob_endpoint
   }
 }
